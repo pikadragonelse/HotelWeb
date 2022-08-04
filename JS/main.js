1842d67d658525a8a058d5ed5ead0655cf2d592d
Bảo Long <28072002long@gmail.com>
2022-08-04 11:24:28 +0700

    done

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,7 @@ const $$ = document.querySelectorAll.bind(document);
 const html = $('html');
 const body = $('body');
 const mainElement = $('.main');
+const modalCloseElement = $('.modal__close-icon');
 const modal = $('.modal');
 const modalOverlay = $('.modal__overlay');
 
@@ -24,6 +25,7 @@ const headerSearchItemElements = $$('.header__search-items');
 
 const headerSearchPlaceElement = $('.header__search-place');
 const headerSearchPlaceOptionElement = $('.header__search-place-option');
+const headerSearchPlaceOptionElement2 = $('.header__search-place-option-2');
 
 const headerSearchReceiveElement = $('.header__search-receive');
 const headerSearchReceiveOptionElement = $('.header__search-receive-option');
@@ -31,11 +33,20 @@ const headerSearchReceiveOptionElement = $('.header__search-receive-option');
 const headerSearchCheckElement = $('.header__search-give');
 const headerSearchCheckOptionElement = $('.header__search-check-option');
 
+const headerSearchCheckStarts = $$('.header__search-check-start');
+const iconCheckeds = $$('.fa-calendar-check');
+const iconChecks = $$('.fa-calendar');
+const timeIns = $$('.header__search-check-time-in');
+
 const headerSearchCustomerElement = $('.header__search-customer');
 const headerSearchCustomerOptionElement = $('.header__search-customer-option')
+const pluses = $$('.fa-plus');
+const minuses = $$('.fa-minus');
+const times = $$('.header__search-customer-times');
 
 const headerSearchButtonMainElement = $('.header__search-button-main');
 const headerSearchButtonTextElement = $('.header__search-button-text');
+
 
 const navbarUserListScrollClass = 'header__navbar-user-bar-list-scroll';
 const headerOptionItemSelected = 'header__navbar-option-items-selected';
@@ -193,11 +204,10 @@ const searchHandle = {
             this.setClassForItemElement(element);
         }
     },
-    placeChoice() {
-        headerSearchPlaceOptionElement.onclick = () => {
+    placeChoice(element) {
+        element.onclick = () => {
 
-            this.checkShow(this.checkPlace, headerSearchPlaceOptionElement);
-
+            this.checkShow(this.checkPlace, element);
             if (!this.checkPlace) {
                 headerSearchPlaceElement.style.display = 'block';
                 this.checkPlace = true;
@@ -221,13 +231,17 @@ const searchHandle = {
 
         }
 
-        headerSearchPlaceOptionElement.onblur = () => {
-            headerSearchPlaceElement.style.display = 'none';
-            this.checkPlace = false;
-            headerSearchPlaceOptionElement.classList.remove(selectedSearchItemClass);
-            
+        window.addEventListener('click', (e) => {
+            if ( !e.target.closest('.header__search-place') && (!e.target.closest('.header__search-place-option'))) { 
+                headerSearchPlaceElement.style.display = 'none';
+                this.checkPlace = false;
+                headerSearchPlaceOptionElement.classList.remove(selectedSearchItemClass);
+            }
 
-        }
+            
+        })
+
+
     },
     checkChoice() {
 
@@ -255,11 +269,13 @@ const searchHandle = {
             }
         }
 
-        headerSearchReceiveOptionElement.onblur = () => {
-            headerSearchReceiveElement.style.display = 'none';
-            this.checkReceive = false;
-            headerSearchReceiveOptionElement.classList.remove(selectedSearchItemClass);
-        }
+        window.addEventListener('click', (e) => {
+            if ( !e.target.closest('.header__search-receive') && !e.target.closest('.header__search-receive-option')) { 
+                headerSearchReceiveElement.style.display = 'none';
+                this.checkReceive = false;
+                headerSearchReceiveOptionElement.classList.remove(selectedSearchItemClass);
+            }
+        })
 
         headerSearchCheckOptionElement.onclick = () => {
             this.checkShow(this.checkCheck, headerSearchCheckOptionElement);
@@ -286,13 +302,13 @@ const searchHandle = {
             }
         }
 
-        headerSearchCheckOptionElement.onblur = () => {
-            headerSearchCheckElement.style.display = 'none';
-            this.checkCheck = false;
-            headerSearchCheckOptionElement.classList.remove(selectedSearchItemClass);
-            
-
-        }
+        window.addEventListener('click',  (e) => {
+            if ( !e.target.closest('.header__search-give') && !e.target.closest('.header__search-check-option')) { 
+                headerSearchCheckElement.style.display = 'none';
+                this.checkCheck = false;
+                headerSearchCheckOptionElement.classList.remove(selectedSearchItemClass);
+            }
+        })
 
     },
     customerChoice() {
@@ -323,15 +339,104 @@ const searchHandle = {
             }
         }
 
-        headerSearchCustomerOptionElement.onblur = () => {
-            headerSearchCustomerElement.style.display = 'none';
-            this.checkCustomer = false;
-
-            headerSearchCustomerOptionElement.classList.remove(selectedSearchItemClass);
-            
-        }
+        window.addEventListener('click', (e) => {
+            if ( !e.target.closest('.header__search-customer') && !e.target.closest('.header__search-customer-option')) { 
+                headerSearchCustomerElement.style.display = 'none';
+                this.checkCustomer = false;
+                headerSearchCustomerOptionElement.classList.remove(selectedSearchItemClass);
+            }
+        })
     },
 
+
+}
+
+const handleModal = {
+    closeModal() {
+        modalCloseElement.onclick = () => {
+            modal.style.display = 'none';
+        }
+
+        window.addEventListener('keyup', (e) => {
+            if (e.keyCode === 27)
+                modal.style.display = 'none';
+        })
+    }
+}
+
+const handelOptions = {
+    selectedCheckStart() {
+        for (let element of headerSearchCheckStarts) {
+            element.onclick = () => {
+                if (!Object.values(element.classList).find((classElement) => {return classElement === 'header__search-check-start--selected'})) {
+                    element.classList.add('header__search-check-start--selected');
+                    for (let icon of iconCheckeds) {
+                        if (icon.parentNode === element) {
+                            icon.style.display = 'inline-block';
+                        }
+                    }
+
+                    for (let icon of iconChecks) {
+                        if (icon.parentNode === element) {
+                            icon.style.display = 'none';
+                        }
+                    }
+                }
+                else {
+                    element.classList.remove('header__search-check-start--selected');
+                    for (let icon of iconCheckeds) {
+                        if (icon.parentNode === element) {
+                            icon.style.display = 'none';
+                        }
+                    }
+
+                    for (let icon of iconChecks) {
+                        if (icon.parentNode === element) {
+                            icon.style.display = 'inline-block';
+                        }
+                    }
+                }
+            }
+        }
+
+    },
+    selectedTimeIn() {
+        for (let time of timeIns) {
+            time.onclick = () => {
+                
+                if (!Object.values(time.classList).find((classElement) => {return classElement === 'header__search-check-time-in--selected'})) {
+                    time.classList.add('header__search-check-time-in--selected');
+                }
+                else {
+                    time.classList.remove('header__search-check-time-in--selected');
+                }
+            }
+        }
+    },
+    handlePlusMinor() {
+        for (let plus of pluses) {
+            plus.onclick = () => {
+                 $('#T' + plus.id).innerText = Number($('#T' + plus.id).innerText) + 1;
+                 if (Number($('#T' + plus.id).innerText) > 0) {
+                    $('#M' + plus.id).classList.remove('header__search-customer-icon--disable');
+                    $('#M' + plus.id).classList.add('header__search-customer-icon--enable');
+                 }
+            }
+        }
+
+        for (let minus of minuses) {
+            minus.onclick = () => {
+                if (Object.values(minus.classList).find((classElement) => {return classElement === 'header__search-customer-icon--enable'}) && Number($('#T' + minus.id.split('')[1]).innerText) > 0) {
+                    $('#T' + minus.id.split('')[1]).innerText = Number($('#T' + minus.id.split('')[1]).innerText) - 1;
+                }
+                if (Number($('#T' + minus.id.split('')[1]).innerText) === 0) {
+                    minus.classList.add('header__search-customer-icon--disable');
+                    minus.classList.remove('header__search-customer-icon--enable');
+                }
+            }
+
+        }
+    }
 }
 
 function start() {
@@ -340,9 +445,14 @@ function start() {
     handleUser.userRegister();
     handleUser.userLogin();
     scrollHandle.changeNavbar();
-    searchHandle.placeChoice();
+    searchHandle.placeChoice(headerSearchPlaceOptionElement);
+    searchHandle.placeChoice(headerSearchPlaceOptionElement2);
     searchHandle.checkChoice();
     searchHandle.customerChoice();
+    handleModal.closeModal();
+    handelOptions.selectedCheckStart();
+    handelOptions.selectedTimeIn();
+    handelOptions.handlePlusMinor();
 }
 
 
